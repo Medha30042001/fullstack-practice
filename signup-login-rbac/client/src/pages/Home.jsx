dbee19f3d2d855f4dceaee4b59a3465a06e60f1a
Medha Adepu <medha.adepucse@gmail.com>
2026-02-20 20:37:43 +0530

    frontend 2

--- signup-login-rbac/client/src/pages/Home.jsx
+++ signup-login-rbac/client/src/pages/Home.jsx
@@ -1,20 +1,49 @@
-import axios from 'axios';
-import React, { useEffect } from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
 
 const Home = () => {
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("Hello");
 
-    useEffect(() => {
-        axios.get(`${import.meta.env.VITE_API_URL}/auth/home`, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem("token")}`
-            }
-        })
-        .then(res => console.log(res.data))
-        .catch(err => console.log(err.response?.data))
-    }, []);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      setError("Not logged in");
+      return;
+    }
+
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/auth/home`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err.response?.data));
+  }, []);
+
+  if (error) {
+    return <div className="p-4 text-red-500">{error}</div>;
+  }
+
   return (
-    <div>Home</div>
-  )
-}
+    <>
 
-export default Home
+    <div className="p-4 text-xl">
+        {message || "Loading..."}
+    </div>
+
+      <button
+        onClick={() => {
+          localStorage.removeItem("token");
+          window.location.reload();
+        }}
+      >
+        Logout
+      </button>
+    </>
+  );
+};
+
+export default Home;
